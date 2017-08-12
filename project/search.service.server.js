544bed0c5b360fae0698605b1ae711b871b5c51d
amitmangotra06 <cammy@Amits-MacBook-Pro.local>
2017-08-12 09:19:30 -0400

    tried proxy server for POC

--- project/search.service.server.js
+++ project/search.service.server.js
@@ -3,7 +3,9 @@ var app = require("../express");
 var q = require("q");
 const https = require("https");
 
-app.get("kjsdfhsjhd", searchEvent);
+app.get('/find/events', searchEvent);
+
+var appKey = "162d592f5c53704651484b4c2c104f23";
 
 function searchEvent(req, res) {
     var text = req.body;
@@ -18,27 +20,18 @@ function searchEvent(req, res) {
 function apiSearchQuery(text) {
     var deferred = q.defer();
     https.get({
-        host: ''
-    });
-}
-
-
-function oxfordSearchQuery(language, word) {
-    var deferred = q.defer();
-    https.get({
-        host: 'od-api.oxforddictionaries.com',
-        path: '/api/v1/entries/'+language+'/'+word,
+        host: 'api.meetup.com',
+        path: '/find/events?&sign=true&format=json&text="'+text,
         headers: {
             "Accept": "application/json",
-            "app_id": appId,
             "app_key": appKey
         }
-    }, function(response) {
+    }, function (response) {
         var body = '';
-        response.on('data', function(d) {
+        response.on('data', function (d) {
             body += d;
         });
-        response.on('end', function() {
+        response.on('end', function () {
             try {
                 body = JSON.parse(body);
                 deferred.resolve(body);
@@ -49,3 +42,31 @@ function oxfordSearchQuery(language, word) {
     });
     return deferred.promise;
 }
+
+
+// function oxfordSearchQuery(language, word) {
+//     var deferred = q.defer();
+//     https.get({
+//         host: 'od-api.oxforddictionaries.com',
+//         path: '/api/v1/entries/'+language+'/'+word,
+//         headers: {
+//             "Accept": "application/json",
+//             "app_id": appId,
+//             "app_key": appKey
+//         }
+//     }, function(response) {
+//         var body = '';
+//         response.on('data', function(d) {
+//             body += d;
+//         });
+//         response.on('end', function() {
+//             try {
+//                 body = JSON.parse(body);
+//                 deferred.resolve(body);
+//             } catch(e) {
+//                 deferred.reject({error: e});
+//             }
+//         });
+//     });
+//     return deferred.promise;
+// }
--- public/Proof of Concept/search.service.client.js
+++ public/Proof of Concept/search.service.client.js
@@ -13,11 +13,11 @@
 
         function searchEvent(text) {
             // var key = "162d592f5c53704651484b4c2c104f23";
-            var token = "IJX2AX72RMSXZK4VKD77";
-            var clientKey = "4K6ZH2TUDJED4E6IWZ";
-            var url = "https://www.eventbrite.com/oauth/authorize?response_type="+token+"&client_id=";
-            url += clientKey + "/";
-            // var url = "https://api.meetup.com/find/events?&sign=true&format=json&key="+key+"&text="+text;
+            // var token = "IJX2AX72RMSXZK4VKD77";
+            // var clientKey = "4K6ZH2TUDJED4E6IWZ";
+            // var url = "https://www.eventbrite.com/oauth/authorize?response_type="+token+"&client_id=";
+            // url += clientKey + "/";
+            var url = "https://api.meetup.com/find/events?&sign=true&format=json&text="+text;
             // var url = "https://api.meetup.com/find/events?sign=true&sig_id=219882901&key="+key+"&text=" + text;
             //https://api.meetup.com/find/events?&sign=true&photo-host=public&text=beer
             //https://api.meetup.com/find/events?photo-host=public&text=beer&sig_id=219882901&sig=aa19c7d65fe5a765dc78fc85e9751c6611d32e1c
